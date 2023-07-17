6c3a3c3f64227010533f23bd0875867f7daac753
shaorongqiang <shaorongqiang@gmail.com>
2023-07-17 11:50:20 +0800

    update color matching

--- apps/block_scout_web/assets/js/app_extra.js
+++ apps/block_scout_web/assets/js/app_extra.js
@@ -1,6 +1,6 @@
 import { isDarkMode } from './lib/dark_mode'
 
-function applyDarkMode () {
+function applyDarkMode() {
   if (isDarkMode()) {
     document.body.className += ' ' + 'dark-theme-applied'
     document.body.style.backgroundColor = '#1c1d31'
@@ -10,7 +10,7 @@ window.onload = applyDarkMode()
 
 if (isDarkMode()) {
   if (document.getElementById('top-navbar')) {
-    document.getElementById('top-navbar').style.backgroundColor = '#282945'
+    document.getElementById('top-navbar').style.backgroundColor = '#212121'
   }
   if (document.getElementById('navbar-logo')) {
     document.getElementById('navbar-logo').style.filter = 'brightness(0) invert(1)'
@@ -23,11 +23,11 @@ if (isDarkMode()) {
   const search = document.getElementById('main-search-autocomplete')
   const searchMobile = document.getElementById('main-search-autocomplete-mobile')
   if (search && search.style) {
-    search.style.backgroundColor = '#22223a'
-    search.style.borderColor = '#22223a'
+    search.style.backgroundColor = '#1A1A1A'
+    search.style.borderColor = '#1A1A1A'
   }
   if (searchMobile && searchMobile.style) {
-    searchMobile.style.backgroundColor = '#22223a'
-    searchMobile.style.borderColor = '#22223a'
+    searchMobile.style.backgroundColor = '#1A1A1A'
+    searchMobile.style.borderColor = '#1A1A1A'
   }
 }
